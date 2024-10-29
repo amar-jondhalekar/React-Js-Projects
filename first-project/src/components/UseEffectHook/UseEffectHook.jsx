c82fd8969013c1ed9b1a0fd66625038592c1d495
Amar Jondhalekar <amarjondhalekar221297@gmail.com>
2024-10-29 11:32:29 +0530

    useEffect using real time api calling example

--- first-project/src/components/UseEffectHook/UseEffectHook.jsx
+++ first-project/src/components/UseEffectHook/UseEffectHook.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const UseEffectHook = () => {
+    const [text, setText] = useState("posts");
+    const [data, setData] = useState(null);
+    console.log(text);
+    useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/${text}`)
+        .then((response) => response.json())
+        .then((json) => setData(json));
+    }, [text])
+
+    console.log(data);
   return (
-    <div>UseEffectHook</div>
+    <div>
+      <h2>useEffect</h2>
+        <button onClick={() => { setText('posts')}}>Posts</button>
+        <button onClick={() => { setText('users')}}>Users</button>
+        <button onClick={() => { setText('comments')}}>Comments</button>
+        {
+          data?.map((value, i) => (
+            <pre key={i}>
+              {JSON.stringify(value)}
+            </pre>
+          ))
+        }
+    </div>
   )
 }
 
