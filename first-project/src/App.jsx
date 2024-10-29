828ed8e62b2cc50f706f5e43c89a888c2ce431c7
Amar Jondhalekar <amarjondhalekar221297@gmail.com>
2024-10-29 08:25:23 +0530

    created function clickMe and then give reference

--- first-project/src/App.jsx
+++ first-project/src/App.jsx
@@ -5,6 +5,9 @@ import Footer from "./components/Footer";
 import "./App.css"
 
 function App() {
+  function clickMe(){
+    console.log('hey!!!');
+  }
   return(
     <div>
       <Navigation/>
@@ -20,6 +23,10 @@ function App() {
       <h2 onClick={() => {
         console.log('Hi');
       }}> Click </h2>
+      <h1 onClick={clickMe}>
+        ClickMe!
+      </h1>
+      
       </div>
       <Footer/>
     </div>
