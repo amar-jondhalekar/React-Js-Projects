a5024fd4360514461f8d66dae0d97a6863b864eb
Amar Jondhalekar <amarjondhalekar221297@gmail.com>
2024-10-31 11:23:43 +0530

    imported useMemo hook

--- react-hooks/src/App.jsx
+++ react-hooks/src/App.jsx
@@ -1,5 +1,7 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import './App.css'
+import UseStateHook from './ReactHooks/UseStateHook'
+import UseMemoHook from './ReactHooks/UseMemoHook'
 
 // function countInitial(){
 //   console.log('run function');
@@ -7,35 +9,11 @@ import './App.css'
 // }
 
 function App() {
-  const [state, setState] = useState({count: 4, theme: 'blue'})
-  const count = state.count
-  const theme = state.theme
-
-  function incremenCount(){
-    // setCount(prevcount => prevcount + 1);
-    // console.log(count);
-    setState(prevState => {
-      return{
-        ...prevState, count: prevState.count + 1
-      }
-    })
-  }
-  function decrementCount(){
-    // setCount(prevcount => prevcount - 1);
-    // console.log(count);
-    setState(prevState => {
-      return {
-        ...prevState, count: prevState.count - 1
-      }
-    })
-  }
-
-  return (
+  return(
     <>
-     <button onClick={decrementCount}>-</button>
-     <span>{count}</span>
-     <span>{theme}</span>
-     <button onClick={incremenCount}>+</button>
+    <div> React Hooks </div>
+    <UseStateHook/>
+    <UseMemoHook/>
     </>
   )
 }
