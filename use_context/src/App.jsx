ae43594b2842bc2307c46bbe96756f36b42d7788
Amar Jondhalekar <amarjondhalekar221297@gmail.com>
2024-10-31 15:46:31 +0530

    create context

---
+++ use_context/src/App.jsx
@@ -0,0 +1,15 @@
+import { createContext } from 'react'
+import './App.css'
+
+// create context 
+const UserContext = createContext();
+
+function App(){
+  return(
+    <>
+    <div> Hello </div>
+    </>
+  )
+}
+
+export default App
