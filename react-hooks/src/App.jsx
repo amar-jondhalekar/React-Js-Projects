66fcf49f09ecd846754baff5633d2a7969f88436
Amar Jondhalekar <amarjondhalekar221297@gmail.com>
2024-10-31 09:42:26 +0530

    added object to useState

--- react-hooks/src/App.jsx
+++ react-hooks/src/App.jsx
@@ -1,22 +1,40 @@
 import { useState } from 'react'
 import './App.css'
 
+// function countInitial(){
+//   console.log('run function');
+//   return 4;
+// }
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [state, setState] = useState({count: 4, theme: 'blue'})
+  const count = state.count
+  const theme = state.theme
 
   function incremenCount(){
-    setCount(prevcount => prevcount + 1);
-    console.log(count);
+    // setCount(prevcount => prevcount + 1);
+    // console.log(count);
+    setState(prevState => {
+      return{
+        ...prevState, count: prevState.count + 1
+      }
+    })
   }
   function decrementCount(){
-    setCount(prevcount => prevcount - 1);
-    console.log(count);
+    // setCount(prevcount => prevcount - 1);
+    // console.log(count);
+    setState(prevState => {
+      return {
+        ...prevState, count: prevState.count - 1
+      }
+    })
   }
 
   return (
     <>
      <button onClick={decrementCount}>-</button>
      <span>{count}</span>
+     <span>{theme}</span>
      <button onClick={incremenCount}>+</button>
     </>
   )
