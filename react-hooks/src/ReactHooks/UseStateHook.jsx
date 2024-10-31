71c6fdb1a6609be6d6bed4ca8bc4be9d7fe33e28
Amar Jondhalekar <amarjondhalekar221297@gmail.com>
2024-10-31 11:23:57 +0530

    small changes

---
+++ react-hooks/src/ReactHooks/UseStateHook.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { useState } from 'react'
+
+const UseStateHook = () => {
+    const [state, setState] = useState({count: 4, theme: 'blue'})
+    const count = state.count
+    const theme = state.theme
+  
+    function incremenCount(){
+      // setCount(prevcount => prevcount + 1);
+      // console.log(count);
+      setState(prevState => {
+        return{
+          ...prevState, count: prevState.count + 1
+        }
+      })
+    }
+    function decrementCount(){
+      // setCount(prevcount => prevcount - 1);
+      // console.log(count);
+      setState(prevState => {
+        return {
+          ...prevState, count: prevState.count - 1
+        }
+      })
+    }
+  
+    return (
+      <>
+       <button onClick={decrementCount}>-</button>
+       <span>{count}</span>
+       <span>{theme}</span>
+       <button onClick={incremenCount}>+</button>
+      </>
+    )
+}
+
+export default UseStateHook
