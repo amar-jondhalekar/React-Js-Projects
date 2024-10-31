ec3aec3438af5d0863c4ae53edb1ac61fede42f7
Amar Jondhalekar <amarjondhalekar221297@gmail.com>
2024-10-31 12:30:23 +0530

    Updated the ref with the current count after each render

--- react-hooks/src/ReactHooks/UseRefHook.jsx
+++ react-hooks/src/ReactHooks/UseRefHook.jsx
@@ -4,18 +4,27 @@ const UseRefHook = () => {
     const [text, setText] = useState("");
     // const [rendercount, setRenderCount] = useState(0);
     const renderCount = useRef(0);
+    const inputRef = useRef();
+    const prevName = useRef('');
 
+    function focus(){
+        inputRef.current.focus();
+    }
     useEffect(() => {
         // setRenderCount(prevRenderCount => prevRenderCount + 1);
         renderCount.current = renderCount.current + 1;
+        prevName.current = text
 
-    })
+    }, [text])
   return (
     <>
     <h2> UseRef Hook </h2>
     <input type="text" value={text} onChange={(e) => setText(e.target.value)}/>
+    <input type="text" value={text} ref={inputRef} onChange={(e) => setText(e.target.value)}/>
     <h4>My Name is: {text}</h4>
     <div> I rendered {renderCount.current} times </div>
+    <button onClick={focus}> Focus </button>
+    <div> My name is {text} and my previous name is {prevName.current}</div>
     </>
   )
 }
