4ce9eb9300ca65ce44114479bdf5d36898387d52
Amar Jondhalekar <amarjondhalekar221297@gmail.com>
2024-10-31 11:58:23 +0530

    added dependency of number and text only re-runs when 'number' and 'text' changes

--- react-hooks/src/ReactHooks/UseMemoHook.jsx
+++ react-hooks/src/ReactHooks/UseMemoHook.jsx
@@ -1,13 +1,22 @@
-import React, { useState } from 'react'
+import React, { useMemo, useState } from 'react'
 
 const UseMemoHook = () => {
   const [number, setNumber] = useState(1);
   const [text, setText] = useState('');
 
+  // Using useMemo to memoize the result of the calculation
+  const largeCalculation = useMemo(() => {
+    console.log("Calculating...");
+    let sum = 0;
+    for(let i = 0; i <= 1000000; i++){
+      sum += number;
+    }
+    return sum;
+  }, [number, text]) // Only re-runs when 'number' changes
   // using 
   return (
     <>
-    <h2> Expensive Calculate Result: </h2>
+    <h2> Expensive Calculate Result: {largeCalculation}</h2>
     <input type="number" value={number} onChange={(e) => setNumber(parseInt(e.target.value))}/>
     <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder='type here without delay'/>
     </>
